45a530519f0c55dfd7fc16e8b0f73b7f08b17cff
saranataimm <saranatai.10123@gmail.com>
2021-11-24 10:30:02 +0700

    LAB2

--- LAB1/es6-2-array.js
+++ LAB1/es6-2-array.js
@@ -120,3 +120,30 @@ function over15fn(value) {
     return value > 15;
 }
 //
+
+let sum = numbs.reduce(addNumbs);
+console.log(sum);
+
+function addNumbs(total,value) {
+    return total + value;
+}
+//check every value in array => true / false
+let over10 = numbs.every(over10fn);
+console.log(over10);
+
+function over18fn(value) {
+    return value > 10;
+}
+
+let someOver10 = numbs.some(over10fn);
+console.log(someOver10);
+
+let first = numbs.find(over10fn);
+console.log(first);
+
+let firstIndex = numbs.findIndex(over10fn);
+console.log(firstIndex);
+
+const alphabet = "ABCDEF";
+let arrAlph = Array.from(alphabet);
+console.log(arrAlph);
---
+++ readme.md
@@ -0,0 +1,25 @@
+## 4134901 : Special Problem in Information Technology
+
+*รายวิชาหัวข้อพิเศษด้านเทคโนโลยีสารสนเทศ*
+_สาขาวิชาเทคโนโลยีสารสนเทศ_
+**คณะวิทยาศาสตร์**
+
+### นายสรณไตร ไขลี
+
+![นายสรณไตร ไขลี](https://i.pinimg.com/564x/e4/e3/5a/e4e35ad8c1006995b3bca19e66ac8fd1.jpg)
+>รหัสนักศึกษา 610112418029
+
+###Link LAB
+[LAB1](https://github.com/saranataimm/4134901-2-64/tree/master/LAB1)
+[LAB2](https://github.com/saranataimm/4134901-2-64/tree/master/LAB2)
+
+###RUN
+**LAB1**
+
+> npm install
+```
+    const person = {firstName: "Min",age:22}
+```
+**LAB2**
+
+> npm update
